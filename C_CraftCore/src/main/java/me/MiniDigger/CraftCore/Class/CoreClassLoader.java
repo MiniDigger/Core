8ecc5e774b525d6565a9300d51853aba5a06b993
MiniDigger <mini.digger@live.de>
2014-09-05 21:08:07 +0200

    Restore "lost" classes

---
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Class/CoreClassLoader.java
@@ -0,0 +1,55 @@
+/**
+ *
+ * ██████████████████████████████████████████████████████████████████████████████████████████████████████████████
+ * █░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░░░░░░░░░█
+ * █░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █░░░░░░▄▀░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░████░░▄▀░░███░░▄▀░░█████████
+ * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████████
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░█░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █████░░░░░░█████░░░░░░██░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░░░░░█
+ * ██████████████████████████████████████████████████████████████████████████████████████████████████████████████
+ */
+
+package me.MiniDigger.CraftCore.Class;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import me.MiniDigger.Core.Core;
+import me.MiniDigger.Core.Class.ClassLoader;
+
+public class CoreClassLoader implements ClassLoader {
+	
+	@Override
+	public Class<?> load(final URL classUrl, final String name) {
+		final URL[] classUrls = { classUrl };
+		final URLClassLoader ucl = new URLClassLoader(classUrls);
+		Class<?> c;
+		try {
+			System.out.println(name);
+			c = ucl.loadClass(name);
+			ucl.close();
+		} catch (ClassNotFoundException | IOException e) {
+			Core.getCore().getInstance().error("Could not load class " + name + " from repo (" + classUrl.toExternalForm() + ")!");
+			e.printStackTrace();
+			return null;
+		}
+		try {
+			ucl.close();
+		} catch (final IOException e) {
+			Core.getCore().getInstance().error("Error while loading class " + name + " !");
+			e.printStackTrace();
+			return null;
+		}
+		
+		Core.getCore().getInstance().info("Class" + c.getPackage().getName() + "." + c.getSimpleName() + " loaded");
+		return c;
+	}
+	
+}
