8271d5d663d84ebe4df14eb2d6c1acdfa0fee0d9
MiniDigger <mini.digger@live.de>
2014-09-21 11:23:18 +0200

    Add GetTheDrop command

--- C_GetTheDrop/src/main/java/me/MiniDigger/Core/AddOn/GetTheDrop/GetTheDropAddOn.java
+++ C_GetTheDrop/src/main/java/me/MiniDigger/Core/AddOn/GetTheDrop/GetTheDropAddOn.java
@@ -15,8 +15,17 @@
  */
 package me.MiniDigger.Core.AddOn.GetTheDrop;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+
+import me.MiniDigger.Core.Core;
+import me.MiniDigger.Core.Command.Command;
+import me.MiniDigger.Core.Command.CommandArgs;
+import me.MiniDigger.Core.Event.Events.UserJoinGameEvent;
 import me.MiniDigger.Core.Game.GameType;
+import me.MiniDigger.Core.User.User;
 import me.MiniDigger.CraftCore.AddOn.CoreAddOn;
+import me.MiniDigger.CraftCore.Event.Events.CoreUserJoinGameEvent;
 
 public class GetTheDropAddOn extends CoreAddOn {
 	
@@ -24,5 +33,17 @@ public class GetTheDropAddOn extends CoreAddOn {
 	public void enable() {
 		GameType.GETTHEDROP.setClass(GetTheDropGame.class);
 		super.enable();
+	}
+	
+	@Command(name = "getthedrop")
+	public void getthedrop(CommandArgs args) {
+		GetTheDropGame game = new GetTheDropGame();
+		Core.getCore().getGameHandler().addGame(game);
+		for (User user : Core.getCore().getUserHandler().getOnlineUsers()) {
+			UserJoinGameEvent e1 = new CoreUserJoinGameEvent(game, user);
+			Bukkit.getPluginManager().callEvent((Event) e1);
+			game.join(user);
+		}
+		game.start();
 	}
 }
