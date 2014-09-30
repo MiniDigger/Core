978af5cfbe7575c6a4ce3bd3877b3e9721f43cc7
MiniDigger <mini.digger@live.de>
2014-09-30 11:49:36 +0200

    Fuck shutdown

--- C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Util/CoreShutdownUtil.java
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Util/CoreShutdownUtil.java
@@ -39,9 +39,13 @@ import me.MiniDigger.Core.Util.ShutdownUtil;
 import me.MiniDigger.CraftCore.CoreMain;
 import me.MiniDigger.CraftCore.Packet.Packets.ServerPacket;
 import me.MiniDigger.CraftCore.Server.CoreServer;
+import mkremins.fanciful.FancyMessage;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class CoreShutdownUtil implements ShutdownUtil {
@@ -54,19 +58,41 @@ public class CoreShutdownUtil implements ShutdownUtil {
 	
 	@Override
 	public void doShutdown() {
+		initShutdown();
+		
 		for (final Player p : Core.getCore().getUserHandler().getOnlinePlayers()) {
 			Core.getCore().getBarHandler().setBar(p, ChatColor.RED + "Der Server wird nun neugestartet!", 100F);
 			Core.getCore().getUserHandler().get(p.getUniqueId())
 			        .sendMessage(Prefix.API.getPrefix().then("Der Server wird in 10 Sekunden neugestartet!").color(ChatColor.RED));
 		}
+		Prefix.API.getPrefix().then("Der Server wird in 10 Sekunden neugestartet!").color(ChatColor.RED).send(Bukkit.getConsoleSender());
 		
 		Bukkit.getScheduler().runTaskLater((CoreMain) Core.getCore().getInstance(), new Runnable() {
 			
 			@Override
 			public void run() {
 				for (final Player p : Core.getCore().getUserHandler().getOnlinePlayers()) {
+					FancyMessage msg = Prefix.CORE.getPrefix().then("Der Server wird neugestartet!").color(ChatColor.RED).style(ChatColor.BOLD);
+					msg.send(p);
+					msg = Prefix.CORE.getPrefix().then("Er wird gleich wieder da sein!").color(ChatColor.AQUA).style(ChatColor.BOLD);
+					msg.send(p);
+				}
+				
+				for (final Player p : Core.getCore().getUserHandler().getOnlinePlayers()) {
 					p.kickPlayer(ChatColor.RED + "Der Server wird nun neugestarte \n " + ChatColor.AQUA + " Er wird gleich wieder online sein");
 				}
+				
+				for (final World w : Bukkit.getWorlds()) {
+					for (final Entity e : w.getEntities()) {
+						if (e.getType() != EntityType.PLAYER && e.getType() != EntityType.ENDER_CRYSTAL && e.getType() != EntityType.ITEM_FRAME
+						        && e.getType() != EntityType.LEASH_HITCH && e.getType() != EntityType.PAINTING) {
+							e.eject();
+							e.remove();
+						}
+					}
+					w.save();
+				}
+				
 				Bukkit.shutdown();
 			}
 		}, 10 * 20);
