27f54db2d2ae43970950646c7dc1d4dcc2e8ecff
MiniDigger <mini.digger@live.de>
2014-10-21 16:29:14 +0200

    Push pending changes

---
+++ C_BedWars/src/main/java/me/MiniDigger/Core/AddOn/BedWars/TeamBedFeature.java
@@ -0,0 +1,80 @@
+/**
+ * █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+ * █░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░░░░░░░░░█
+ * █░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █░░░░░░▄▀░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░████░░▄▀░░███░░▄▀░░█████████
+ * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████████
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░█░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █████░░░░░░█████░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░░░░░█
+ * █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+ */
+package me.MiniDigger.Core.AddOn.BedWars;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import me.MiniDigger.Core.Core;
+import me.MiniDigger.Core.Feature.FeatureType;
+import me.MiniDigger.Core.Phase.Phase;
+import me.MiniDigger.Core.Team.Team;
+import me.MiniDigger.CraftCore.Feature.CoreFeature;
+import me.MiniDigger.CraftCore.Feature.Features.TeamFeature;
+
+/**
+ * @author Martin
+ * 
+ */
+public class TeamBedFeature extends CoreFeature {
+	
+	private HashMap<String, BedFeature>	beds;
+	private int	                        numTeams;
+	
+	public TeamBedFeature(Phase phase, int numTeams) {
+		super(phase);
+		this.numTeams = numTeams;
+	}
+	
+	@Override
+	public FeatureType getType() {
+		return FeatureType.TEAM_BED;
+	}
+	
+	@Override
+	public List<FeatureType> getDependencies() {
+		return new ArrayList<FeatureType>();
+	}
+	
+	@Override
+	public List<FeatureType> getSoftDependencies() {
+		return new ArrayList<FeatureType>();
+	}
+	
+	@Override
+	public List<FeatureType> getIncompabilities() {
+		return new ArrayList<FeatureType>();
+	}
+	
+	@Override
+	public void start() {
+		beds = new HashMap<String, BedFeature>();
+		
+		List<Team> teams = ((TeamFeature) getPhase().getFeature(FeatureType.TEAM)).getTeams();
+		
+		for (int i = 0; i < numTeams; i++) {
+			BedFeature f = new BedFeature(getPhase(), Core.getCore().getLocationUtil().StringToLocation(teams.get(i).getData("bed_loc")), teams.get(i).getName());
+			beds.put(teams.get(i).getName(), f);
+		}
+	}
+	
+	@Override
+	public void end() {
+		
+	}
+	
+}
