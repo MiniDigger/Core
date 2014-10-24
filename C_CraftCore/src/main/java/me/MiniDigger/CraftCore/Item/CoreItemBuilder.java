ee6b6c37639c6d44a3409d4311f22900b07d9b3e
MiniDigger <mini.digger@live.de>
2014-10-24 18:35:32 +0200

    Adds a chainable item builder! I am having a good day!

---
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Item/CoreItemBuilder.java
@@ -0,0 +1,113 @@
+/**
+ * █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+ * █░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░░░░░░░░░█
+ * █░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █░░░░░░▄▀░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░████░░▄▀░░███░░▄▀░░█████████
+ * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████████
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░█░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █████░░░░░░█████░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░░░░░█
+ * █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+ */
+package me.MiniDigger.CraftCore.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.MiniDigger.Core.Item.ItemBuilder;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+public class CoreItemBuilder implements ItemBuilder {
+	
+	private ItemStack	is;
+	
+	public CoreItemBuilder(Material mat) {
+		is = new ItemStack(mat);
+	}
+	
+	@Override
+	public ItemBuilder amount(int amount) {
+		is.setAmount(amount);
+		return this;
+	}
+	
+	@Override
+	public ItemBuilder name(String name) {
+		ItemMeta meta = is.getItemMeta();
+		meta.setDisplayName(name);
+		is.setItemMeta(meta);
+		return this;
+	}
+	
+	@Override
+	public ItemBuilder lore(String name) {
+		ItemMeta meta = is.getItemMeta();
+		List<String> lore = meta.getLore();
+		lore.add(name);
+		meta.setLore(lore);
+		is.setItemMeta(meta);
+		return this;
+	}
+	
+	@Override
+	public ItemBuilder durability(short durability) {
+		is.setDurability(durability);
+		return this;
+	}
+	
+	@Override
+	@SuppressWarnings("deprecation")
+	public ItemBuilder data(byte data) {
+		is.setData(new MaterialData(is.getType(), data));
+		return this;
+	}
+	
+	@Override
+	public ItemBuilder enchantment(Enchantment enchantment, int level) {
+		is.addUnsafeEnchantment(enchantment, level);
+		return this;
+	}
+	
+	@Override
+	public ItemBuilder enchantment(Enchantment enchantment) {
+		is.addUnsafeEnchantment(enchantment, 1);
+		return this;
+	}
+	
+	@Override
+	public ItemBuilder type(Material material) {
+		is.setType(material);
+		return this;
+	}
+	
+	@Override
+	public ItemBuilder clearLore() {
+		ItemMeta meta = is.getItemMeta();
+		meta.setLore(new ArrayList<String>());
+		is.setItemMeta(meta);
+		return this;
+	}
+	
+	@Override
+	public ItemBuilder clearEnchantments(){
+		for(Enchantment e : is.getEnchantments().keySet()){
+			is.removeEnchantment(e);
+		}
+		return this;
+	}
+	
+	@Override
+	public ItemStack build() {
+		return is;
+	}
+	
+}
