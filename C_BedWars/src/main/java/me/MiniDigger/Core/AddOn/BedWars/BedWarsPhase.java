28e5d080d9a9b452a4a98467a59b8e9ed56f7ab5
MiniDigger <mini.digger@live.de>
2014-10-29 18:51:46 +0100

    Fix trades...

--- C_BedWars/src/main/java/me/MiniDigger/Core/AddOn/BedWars/BedWarsPhase.java
+++ C_BedWars/src/main/java/me/MiniDigger/Core/AddOn/BedWars/BedWarsPhase.java
@@ -55,6 +55,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Villager.Profession;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffectType;
@@ -90,7 +91,9 @@ public class BedWarsPhase extends CorePhase {
 		addFeature(new BleedFeature(this));
 		addFeature(new AutoRespawnFeature(this));
 		addFeature(new FixedFoodFeature(this));
-		addFeature(new MobFeature(this, Core.getCore().getEntityUtil().getAll(Type.OTHER, Type.CART, Type.PROJECTILE, Type.UTILITY)));
+		List<EntityType> allowed = Core.getCore().getEntityUtil().getAll(Type.OTHER, Type.CART, Type.PROJECTILE, Type.UTILITY);
+		allowed.add(EntityType.VILLAGER);
+		addFeature(new MobFeature(this, allowed));
 		addFeature(new FixedTimeFeature(this, 12000));
 		addFeature(new FixedWeatherFeature(this, WeatherType.CLEAR));
 		addFeature(new DropFeature(this));
@@ -102,22 +105,22 @@ public class BedWarsPhase extends CorePhase {
 		addFeature(new TeamDeathMatchFeature(this));
 		addFeature(new VillagerFeature(this));
 		
-		final ItemStack silver = new CoreItemBuilder(Material.IRON_INGOT).name(ChatColor.AQUA + "Silber").amount(1).build();
+		ItemStack silver = new CoreItemBuilder(Material.IRON_INGOT).name(ChatColor.AQUA + "Silber").amount(1).build();
 		
-		final ItemStack bronce = new CoreItemBuilder(Material.CLAY_BRICK).name(ChatColor.AQUA + "Bronze").amount(1).build();
+		ItemStack bronce = new CoreItemBuilder(Material.CLAY_BRICK).name(ChatColor.AQUA + "Bronze").amount(1).build();
 		
-		final ItemStack gold = new CoreItemBuilder(Material.GOLD_INGOT).name(ChatColor.AQUA + "Gold").amount(1).build();
+		ItemStack gold = new CoreItemBuilder(Material.GOLD_INGOT).name(ChatColor.AQUA + "Gold").amount(1).build();
 		
 		addFeature(new SpawnerFeature(this, DyeColor.PINK, 20 * 4, null, bronce));
 		addFeature(new SpawnerFeature(this, DyeColor.LIGHT_BLUE, 20 * 15, null, silver));
 		addFeature(new SpawnerFeature(this, DyeColor.MAGENTA, 20 * 60, null, gold));
 		
-		genFoodVillager(DyeColor.ORANGE, bronce, silver, gold, 4);
-		genArmorVillager(DyeColor.ORANGE, bronce, silver, gold, 3);
-		genBowVillager(DyeColor.ORANGE, bronce, silver, gold, 1);
-		genWeaponVillager(DyeColor.ORANGE, bronce, silver, gold, 2);
-		genBlockVillager(DyeColor.ORANGE, bronce, silver, gold, 5);
-		genPotionVillager(DyeColor.ORANGE, bronce, silver, gold, 6);
+		genFoodVillager(DyeColor.ORANGE, bronce.clone(), silver.clone(), gold.clone(), 4);
+		genArmorVillager(DyeColor.ORANGE, bronce.clone(), silver.clone(), gold.clone(), 3);
+		genBowVillager(DyeColor.ORANGE, bronce.clone(), silver.clone(), gold.clone(), 1);
+		genWeaponVillager(DyeColor.ORANGE, bronce.clone(), silver.clone(), gold.clone(), 2);
+		genBlockVillager(DyeColor.ORANGE, bronce.clone(), silver.clone(), gold.clone(), 5);
+		genPotionVillager(DyeColor.ORANGE, bronce.clone(), silver.clone(), gold.clone(), 6);
 	}
 	
 	private void genPotionVillager(final DyeColor locKey, final ItemStack bronce, final ItemStack silver, final ItemStack gold, final int id) {
@@ -128,22 +131,22 @@ public class BedWarsPhase extends CorePhase {
 		bronce.setAmount(32);
 		is = new CoreItemBuilder(Material.POTION).durability(0).name(ChatColor.LIGHT_PURPLE + "Speed").lore(ChatChars.Misc.bullet + "Causes Hunger")
 		        .effect(PotionEffectType.HUNGER, 90 * 20, 2).effect(PotionEffectType.SPEED, 90 * 20, 2).build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		silver.setAmount(1);
 		is = new CoreItemBuilder(Material.POTION).durability(0).name(ChatColor.LIGHT_PURPLE + "Haste").lore(ChatChars.Misc.bullet + "Causes Hunger")
 		        .effect(PotionEffectType.HUNGER, 90 * 20, 2).effect(PotionEffectType.FAST_DIGGING, 90 * 20).build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(1);
 		is = new CoreItemBuilder(Material.POTION).durability(0).name(ChatColor.LIGHT_PURPLE + "Strength").lore(ChatChars.Misc.bullet + "Causes Hunger")
 		        .effect(PotionEffectType.HUNGER, 90 * 20, 2).effect(PotionEffectType.INCREASE_DAMAGE, 90 * 20).build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(1);
 		is = new CoreItemBuilder(Material.POTION).durability(0).name(ChatColor.LIGHT_PURPLE + "Resistance").lore(ChatChars.Misc.bullet + "Causes Hunger")
 		        .effect(PotionEffectType.HUNGER, 90 * 20, 2).effect(PotionEffectType.ABSORPTION, 90 * 20).build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		is = new CoreItemBuilder(Material.POTION).durability(0).name(ChatColor.RED + "" + ChatColor.BOLD + "Beast Mode")
 		        .effect(PotionEffectType.NIGHT_VISION, 2 * 60 * 20).effect(PotionEffectType.SPEED, 2 * 60 * 20, 2).effect(PotionEffectType.FAST_DIGGING, 2 * 60 * 20)
@@ -174,38 +177,38 @@ public class BedWarsPhase extends CorePhase {
 		
 		bronce.setAmount(8);
 		is = new CoreItemBuilder(Material.NETHERRACK).amount(32).name(ChatColor.GOLD + "Block").lore(" " + ChatChars.Misc.bullet + " Level 1").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		bronce.setAmount(16);
 		is = new CoreItemBuilder(Material.BRICK).amount(16).name(ChatColor.GOLD + "Block").lore(" " + ChatChars.Misc.bullet + " Level 2").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		bronce.setAmount(32);
 		is = new CoreItemBuilder(Material.EMERALD_BLOCK).amount(8).name(ChatColor.GOLD + "Block").lore(" " + ChatChars.Misc.bullet + " Level 3").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		bronce.setAmount(8);
 		is = new CoreItemBuilder(Material.GLOWSTONE).amount(8).name(ChatColor.GOLD + "Block").lore(" " + ChatChars.Misc.bullet + " Level 4")
 		        .lore(ChatChars.Misc.bullet + "Macht Licht!").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		bronce.setAmount(6);
 		is = new CoreItemBuilder(Material.WOOD_PICKAXE).name(ChatColor.DARK_PURPLE + "Pickaxe").enchantment(Enchantment.ARROW_INFINITE)
 		        .lore(" " + ChatChars.Misc.bullet + " Level 1").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		silver.setAmount(3);
 		is = new CoreItemBuilder(Material.IRON_PICKAXE).name(ChatColor.DARK_PURPLE + "Pickaxe").enchantment(Enchantment.ARROW_INFINITE)
 		        .enchantment(Enchantment.DIG_SPEED).lore(" " + ChatChars.Misc.bullet + " Level 2").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		gold.setAmount(1);
 		is = new CoreItemBuilder(Material.IRON_PICKAXE).name(ChatColor.DARK_PURPLE + "Pickaxe").enchantment(Enchantment.ARROW_INFINITE)
 		        .enchantment(Enchantment.DIG_SPEED, 3).lore(" " + ChatChars.Misc.bullet + " Level 3").build();
-		trades.add(new CoreVillagerTrade(gold, is));
+		trades.add(new CoreVillagerTrade(gold.clone(), is));
 		
 		silver.setAmount(1);
-		trades.add(new CoreVillagerTrade(silver, new ItemStack(Material.CHEST)));
+		trades.add(new CoreVillagerTrade(silver.clone(), new ItemStack(Material.CHEST)));
 		
 		final MapData data = ((MapFeature) getFeature(FeatureType.MAP)).getMap();
 		final HashMap<String, Location> locs = data.getLocs(locKey);
@@ -231,28 +234,28 @@ public class BedWarsPhase extends CorePhase {
 		silver.setAmount(2);
 		is = new CoreItemBuilder(Material.GOLD_SWORD).name(ChatColor.RED + "Sword").enchantment(Enchantment.ARROW_INFINITE)
 		        .lore(" " + ChatChars.Misc.bullet + " Level 1").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(8);
 		is = new CoreItemBuilder(Material.GOLD_SWORD).name(ChatColor.RED + "Sword").enchantment(Enchantment.ARROW_INFINITE).enchantment(Enchantment.DAMAGE_ALL)
 		        .lore(" " + ChatChars.Misc.bullet + " Level 2").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(16);
 		is = new CoreItemBuilder(Material.GOLD_SWORD).name(ChatColor.RED + "Sword").enchantment(Enchantment.ARROW_INFINITE).enchantment(Enchantment.DAMAGE_ALL, 2)
 		        .lore(" " + ChatChars.Misc.bullet + " Level 3").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(48);
 		is = new CoreItemBuilder(Material.GOLD_SWORD).name(ChatColor.RED + "Sword").enchantment(Enchantment.ARROW_INFINITE).enchantment(Enchantment.DAMAGE_ALL, 2)
 		        .enchantment(Enchantment.KNOCKBACK).lore(" " + ChatChars.Misc.bullet + " Level 4").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(6);
-		trades.add(new CoreVillagerTrade(silver, new ItemStack(Material.TNT)));
+		trades.add(new CoreVillagerTrade(silver.clone(), new ItemStack(Material.TNT)));
 		
 		gold.setAmount(1);
-		trades.add(new CoreVillagerTrade(gold, new ItemStack(Material.FLINT_AND_STEEL)));
+		trades.add(new CoreVillagerTrade(gold.clone(), new ItemStack(Material.FLINT_AND_STEEL)));
 		
 		final MapData data = ((MapFeature) getFeature(FeatureType.MAP)).getMap();
 		final HashMap<String, Location> locs = data.getLocs(locKey);
@@ -278,31 +281,31 @@ public class BedWarsPhase extends CorePhase {
 		gold.setAmount(2);
 		is = new CoreItemBuilder(Material.BOW).name(ChatColor.GREEN + "Bow").enchantment(Enchantment.ARROW_INFINITE).lore(" " + ChatChars.Misc.bullet + " Level 1")
 		        .build();
-		trades.add(new CoreVillagerTrade(gold, is));
+		trades.add(new CoreVillagerTrade(gold.clone(), is));
 		
 		gold.setAmount(8);
 		is = new CoreItemBuilder(Material.BOW).name(ChatColor.GREEN + "Bow").enchantment(Enchantment.ARROW_INFINITE).enchantment(Enchantment.ARROW_DAMAGE)
 		        .lore(" " + ChatChars.Misc.bullet + " Level 2").build();
-		trades.add(new CoreVillagerTrade(gold, is));
+		trades.add(new CoreVillagerTrade(gold.clone(), is));
 		
 		gold.setAmount(12);
 		is = new CoreItemBuilder(Material.BOW).name(ChatColor.GREEN + "Bow").enchantment(Enchantment.ARROW_INFINITE).enchantment(Enchantment.ARROW_DAMAGE, 2)
 		        .lore(" " + ChatChars.Misc.bullet + " Level 3").build();
-		trades.add(new CoreVillagerTrade(gold, is));
+		trades.add(new CoreVillagerTrade(gold.clone(), is));
 		
 		gold.setAmount(24);
 		is = new CoreItemBuilder(Material.BOW).name(ChatColor.GREEN + "Bow").enchantment(Enchantment.ARROW_INFINITE).enchantment(Enchantment.ARROW_DAMAGE, 2)
 		        .enchantment(Enchantment.ARROW_KNOCKBACK).lore(" " + ChatChars.Misc.bullet + " Level 4").build();
-		trades.add(new CoreVillagerTrade(gold, is));
+		trades.add(new CoreVillagerTrade(gold.clone(), is));
 		
 		gold.setAmount(1);
-		trades.add(new CoreVillagerTrade(gold, new ItemStack(Material.ARROW)));
+		trades.add(new CoreVillagerTrade(gold.clone(), new ItemStack(Material.ARROW)));
 		
 		silver.setAmount(1);
-		trades.add(new CoreVillagerTrade(silver, new ItemStack(Material.LADDER, 3)));
+		trades.add(new CoreVillagerTrade(silver.clone(), new ItemStack(Material.LADDER, 3)));
 		
 		silver.setAmount(1);
-		trades.add(new CoreVillagerTrade(silver, new ItemStack(Material.WEB, 3)));
+		trades.add(new CoreVillagerTrade(silver.clone(), new ItemStack(Material.WEB, 3)));
 		
 		final MapData data = ((MapFeature) getFeature(FeatureType.MAP)).getMap();
 		final HashMap<String, Location> locs = data.getLocs(locKey);
@@ -329,37 +332,37 @@ public class BedWarsPhase extends CorePhase {
 		bronce.setAmount(1);
 		is = new CoreItemBuilder(Material.LEATHER_HELMET).name(ChatColor.BLUE + "Armor").enchantment(Enchantment.PROTECTION_ENVIRONMENTAL)
 		        .enchantment(Enchantment.ARROW_INFINITE).lore("TeamArmor").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		bronce.setAmount(1);
 		is = new CoreItemBuilder(Material.LEATHER_LEGGINGS).name(ChatColor.BLUE + "Armor").enchantment(Enchantment.PROTECTION_ENVIRONMENTAL)
 		        .enchantment(Enchantment.ARROW_INFINITE).lore("TeamArmor").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		bronce.setAmount(1);
 		is = new CoreItemBuilder(Material.LEATHER_BOOTS).name(ChatColor.BLUE + "Armor").enchantment(Enchantment.PROTECTION_ENVIRONMENTAL)
 		        .enchantment(Enchantment.ARROW_INFINITE).lore("TeamArmor").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		silver.setAmount(2);
 		is = new CoreItemBuilder(Material.CHAINMAIL_CHESTPLATE).name(ChatColor.YELLOW + "Chestplate").enchantment(Enchantment.PROTECTION_ENVIRONMENTAL)
 		        .enchantment(Enchantment.ARROW_INFINITE).lore(" " + ChatChars.Misc.bullet + " Level 1").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(8);
 		is = new CoreItemBuilder(Material.CHAINMAIL_CHESTPLATE).name(ChatColor.YELLOW + "Chestplate").enchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2)
 		        .enchantment(Enchantment.ARROW_INFINITE).lore(" " + ChatChars.Misc.bullet + " Level 2").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(16);
 		is = new CoreItemBuilder(Material.CHAINMAIL_CHESTPLATE).name(ChatColor.YELLOW + "Chestplate").enchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3)
 		        .enchantment(Enchantment.ARROW_INFINITE).lore(" " + ChatChars.Misc.bullet + " Level 3").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		silver.setAmount(48);
 		is = new CoreItemBuilder(Material.CHAINMAIL_CHESTPLATE).name(ChatColor.YELLOW + "Chestplate").enchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 5)
 		        .enchantment(Enchantment.ARROW_INFINITE).lore(" " + ChatChars.Misc.bullet + " Level 4").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		final MapData data = ((MapFeature) getFeature(FeatureType.MAP)).getMap();
 		final HashMap<String, Location> locs = data.getLocs(locKey);
@@ -384,23 +387,23 @@ public class BedWarsPhase extends CorePhase {
 		
 		bronce.setAmount(16);
 		is = new CoreItemBuilder(Material.APPLE).amount(4).name(ChatColor.YELLOW + "Food").lore(" " + ChatChars.Misc.bullet + " Level 1").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		bronce.setAmount(32);
 		is = new CoreItemBuilder(Material.APPLE).amount(2).name(ChatColor.YELLOW + "Food").lore(" " + ChatChars.Misc.bullet + " Level 2").build();
-		trades.add(new CoreVillagerTrade(bronce, is));
+		trades.add(new CoreVillagerTrade(bronce.clone(), is));
 		
 		silver.setAmount(3);
 		is = new CoreItemBuilder(Material.GOLDEN_APPLE).name(ChatColor.YELLOW + "Food").lore(" " + ChatChars.Misc.bullet + " Level 3").build();
-		trades.add(new CoreVillagerTrade(silver, is));
+		trades.add(new CoreVillagerTrade(silver.clone(), is));
 		
 		bronce.setAmount(64);
 		silver.setAmount(1);
-		trades.add(new CoreVillagerTrade(bronce, silver));
+		trades.add(new CoreVillagerTrade(bronce.clone(), silver.clone()));
 		
 		silver.setAmount(16);
 		gold.setAmount(1);
-		trades.add(new CoreVillagerTrade(silver, gold));
+		trades.add(new CoreVillagerTrade(silver.clone(), gold.clone()));
 		
 		final MapData data = ((MapFeature) getFeature(FeatureType.MAP)).getMap();
 		if (data == null) {
