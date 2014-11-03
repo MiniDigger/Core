0f1f701260d136a913c7ad3a9d512c07d75e6367
MiniDigger <mini.digger@live.de>
2014-11-03 20:37:27 +0100

    Licence ;D

---
+++ C_Build/src/test/java/me/MiniDigger/Util/CopyrightFixer.java
@@ -0,0 +1,147 @@
+/**
+ * █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+ * █░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░░░░░░░░░█
+ * █░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █░░░░░░▄▀░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░████░░▄▀░░███░░▄▀░░█████████
+ * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░░░███░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████████
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░█░░▄▀░░░░░░░░░░█
+ * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+ * █████░░░░░░█████░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░░░░░█
+ * █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+ * 
+ * Copyright © MiniDigger and others - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Martin Benndorf <admin@minidigger.me>, 2013-2014 and others
+ */
+package me.MiniDigger.Util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CopyrightFixer {
+	
+	private static final List<String>	newHeader	= new ArrayList<String>();
+	private static final List<String>	included	= new ArrayList<String>();
+	private static int	              added	      = 0;
+	private static int	              changed	  = 0;
+	private static int	              skipped	  = 0;
+	
+	public static void main(String[] args) throws Exception {
+		newHeader.add("/**");
+		newHeader.add(" * █████████████████████████████████████████████████████████████████████████████████████████████████████████████████");
+		newHeader.add(" * █░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░░░░░░░░░█");
+		newHeader.add(" * █░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█");
+		newHeader.add(" * █░░░░░░▄▀░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█");
+		newHeader.add(" * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░████░░▄▀░░███░░▄▀░░█████████");
+		newHeader.add(" * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░░░░░█");
+		newHeader.add(" * █████░░▄▀░░█████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█");
+		newHeader.add(" * █████░░▄▀░░█████░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░░░███░░▄▀░░░░░░░░░░█");
+		newHeader.add(" * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░████████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█████░░▄▀░░█████████");
+		newHeader.add(" * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░█░░▄▀░░░░░░░░░░█");
+		newHeader.add(" * █████░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█");
+		newHeader.add(" * █████░░░░░░█████░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░░░░░█");
+		newHeader.add(" * █████████████████████████████████████████████████████████████████████████████████████████████████████████████████");
+		newHeader.add(" * ");
+		newHeader.add(" * Copyright © MiniDigger and others - All Rights Reserved");
+		newHeader.add(" * Unauthorized copying of this file, via any medium is strictly prohibited");
+		newHeader.add(" * Proprietary and confidential");
+		newHeader.add(" * Written by Martin Benndorf <admin@minidigger.me>, 2013-2014 and others");
+		newHeader.add(" */");
+		
+		included.add(".java");
+		
+		File file = new File("F:/DATA/Dev/bukkit_ws/C_Core");
+		fix(file);
+		
+		System.out.println("************************************");
+		System.out.println("DONE!");
+		System.out.println("Scanned: " + (added + changed + skipped));
+		System.out.println("Added: " + added);
+		System.out.println("Changed: " + changed);
+		System.out.println("Skipped: " + skipped);
+		System.out.println("************************************");
+	}
+	
+	public static void fix(File file) throws Exception {
+		if (file.isDirectory()) {
+			for (File f : file.listFiles()) {
+				fix(f);
+			}
+			return;
+		}
+		
+		boolean found = false;
+		for (String s : included) {
+			if (file.getName().endsWith(s)) {
+				found = true;
+			}
+		}
+		
+		if (!found) {
+			System.out.println("File " + file.getAbsolutePath() + " is excluded!");
+			return;
+		}
+		
+		List<String> lines = new ArrayList<String>();
+		List<String> oldHeader = new ArrayList<String>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line;
+		found = false;
+		while ((line = reader.readLine()) != null) {
+			if (line.startsWith("package")) {
+				found = true;
+			}
+			if (found) {
+				lines.add(line);
+			} else {
+				oldHeader.add(line);
+			}
+		}
+		reader.close();
+		
+		found = false;
+		if (oldHeader.size() == 0) {
+			added++;
+			System.out.println("Adding header to " + file.getAbsolutePath() + "!");
+			found = true;
+		}
+		
+		for (int i = 0; i < oldHeader.size(); i++) {
+			if (!oldHeader.get(i).equalsIgnoreCase(newHeader.get(i))) {
+				found = true;
+				break;
+			}
+		}
+		
+		if (!found) {
+			System.out.println("Skipping " + file.getAbsolutePath() + "!");
+			skipped++;
+			return;
+		}
+		
+		System.out.println("Updating file " + file.getAbsolutePath() + "!");
+		changed++;
+		
+		PrintWriter writer = new PrintWriter(file);
+		
+		for (String s : newHeader) {
+			writer.println(s);
+		}
+		
+		for (String s : lines) {
+			writer.println(s);
+		}
+		
+		writer.close();
+	}
+}
