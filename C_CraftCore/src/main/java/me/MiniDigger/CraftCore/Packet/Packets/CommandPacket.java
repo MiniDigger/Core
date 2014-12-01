cec8b1481e3add712744a6fdd47389cb0fe5097b
MiniDigger <mini.digger@live.de>
2014-12-01 18:47:30 +0100

    Send cmds! Via enter! Yeah!

--- C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Packet/Packets/CommandPacket.java
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Packet/Packets/CommandPacket.java
@@ -18,134 +18,58 @@
  * Proprietary and confidential
  * Written by Martin Benndorf <admin@minidigger.me>, 2013-2014 and others
  */
-package me.MiniDigger.CraftCore.Socket;
+package me.MiniDigger.CraftCore.Packet.Packets;
 
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Pattern;
+
+import org.java_websocket.WebSocket;
 
 import org.bukkit.Bukkit;
 
 import me.MiniDigger.Core.Core;
-import me.MiniDigger.Core.Packet.Packet;
 import me.MiniDigger.Core.Socket.Session;
-import me.MiniDigger.Core.Socket.SocketClient;
-import me.MiniDigger.Core.Socket.SocketHandler;
-import me.MiniDigger.Core.Socket.SocketServer;
 
-import me.MiniDigger.CraftCore.Packet.Packets.ChatPacket;
-import me.MiniDigger.CraftCore.Packet.Packets.CommandPacket;
-import me.MiniDigger.CraftCore.Packet.Packets.IdentificationPacket;
-import me.MiniDigger.CraftCore.Packet.Packets.LogRecordPacket;
-import me.MiniDigger.CraftCore.Packet.Packets.ServerPacket;
+import me.MiniDigger.CraftCore.Packet.CorePacket;
 
-public class CoreSocketHandler implements SocketHandler {
+public class CommandPacket extends CorePacket {
 	
-	private final List<Session>	sessions	= new ArrayList<>();
-	private SocketServer	    server;
-	private SocketClient	    client;
+	private String	toServer;
+	private String	cmd;
 	
 	@Override
-	public void startServer() {
-		server = new CoreSocketServer(new InetSocketAddress(33333));
-		((CoreSocketServer) server).start();
+	public String toString() {
+		return getName() + "|" + toServer + "|" + cmd;
 	}
 	
 	@Override
-	public void startClient() {
-		try {
-			client = new CoreSocketClient(new URI("ws://localhost:33333"));
-			((CoreSocketClient) client).connect();
-			
-			Bukkit.getScheduler().runTaskLater(Core.getCore().getInstance(), new Runnable() {
-				
-				@Override
-				public void run() {
-					final IdentificationPacket packet = new IdentificationPacket();
-					packet.setClientName((Core.getCore().getInstance()).getConfig().getString("ws-user"));
-					packet.setPass((Core.getCore().getInstance()).getConfig().getString("ws-pass"));
-					Core.getCore().getPacketHandler().sendPacket(packet);
-				}
-			}, 10);
-		} catch (final URISyntaxException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	public void registerPackets() {
-		final List<Class<? extends Packet>> classes = new ArrayList<Class<? extends Packet>>();
-		classes.add(ChatPacket.class);
-		classes.add(IdentificationPacket.class);
-		classes.add(ServerPacket.class);
-		classes.add(LogRecordPacket.class);
-		classes.add(CommandPacket.class);
+	public void fromString(final String s) {
+		final String[] ss = s.split(Pattern.quote("|"));
 		
-		for (final Class<? extends Packet> c : classes) {
-			try {
-				Core.getCore().getPacketHandler().registerPacket(c.newInstance().getName(), c);
-			} catch (final Exception e) {
-				e.printStackTrace();
+		toServer = ss[1];
+		cmd = ss[2];
+	}
+	
+	@Override
+	public void handle() {
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+	}
+	
+	@Override
+	public void handle(final WebSocket con) {
+		if (toServer.equalsIgnoreCase("all")) {
+			Core.getCore().getPacketHandler().sendBroadcast(this);
+		} else {
+			Session s = Core.getCore().getSocketHandler().getSession(toServer);
+			if (s == null) {
+				System.out.println("no client called " + toServer);
+				return;
 			}
+			Core.getCore().getPacketHandler().sendPacket(this, s);
 		}
 	}
 	
 	@Override
-	public void openSession(final InetSocketAddress address) {
-		final Session session = new CoreSession(address);
-		sessions.add(session);
-	}
-	
-	@Override
-	public void closeSession(final InetSocketAddress address) {
-		while (getSession(address) != null && sessions.contains(getSession(address))) {
-			sessions.remove(getSession(address));
-		}
-	}
-	
-	@Override
-	public void reciveName(final String name, final InetSocketAddress address) {
-		getSession(address).setName(name);
-	}
-	
-	@Override
-	public Session getSession(final String name) {
-		for (final Session session : sessions) {
-			if (session.getName() == null) {
-				continue;
-			}
-			if (session.getName().equals(name)) {
-				return session;
-			}
-		}
-		return null;
-	}
-	
-	@Override
-	public Session getSession(final InetSocketAddress address) {
-		for (final Session session : sessions) {
-			if (session == null) {
-				continue;
-			}
-			if (session.getAddress() == null) {
-				continue;
-			}
-			if (session.getAddress().equals(address)) {
-				return session;
-			}
-		}
-		return null;
-	}
-	
-	@Override
-	public SocketServer getServer() {
-		return server;
-	}
-	
-	@Override
-	public SocketClient getClient() {
-		return client;
+	public String getName() {
+		return "Command";
 	}
 }
