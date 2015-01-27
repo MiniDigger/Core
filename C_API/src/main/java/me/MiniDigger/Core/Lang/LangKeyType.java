0c0cfbcda9da5e348d8810257a65773c459104b4
MiniDigger <admin@minidigger.me>
2015-01-27 10:37:53 +0100

    Cleanup of LangKeyType

--- C_API/src/main/java/me/MiniDigger/Core/Lang/LangKeyType.java
+++ C_API/src/main/java/me/MiniDigger/Core/Lang/LangKeyType.java
@@ -85,10 +85,10 @@ public class LangKeyType {
 	public static class Cmd {
 		
 		protected static String		    type		  = "cmd";
+		public static final LangKeyType	CANNOT_NULL		= new LangKeyType("cannot_null", type, "`%0%` cannot be null");
 		public static final LangKeyType	EXCEPTION		= new LangKeyType("exception", type, "Unhandled exception executing command `%0%` in plugin `%1%`!");
 		public static final LangKeyType	EXCEPTION_TAB	= new LangKeyType("exception_tab", type,
 		                                                      "Unhandled exception during tab completion for command `%0%` in plugin `%1%`");
-		public static final LangKeyType	CANNOT_NULL		= new LangKeyType("cannot_null", type, "`%0%` cannot be null");
 		
 		public static List<LangKeyType> values() {
 			final List<LangKeyType> values = new ArrayList<>();
@@ -99,44 +99,117 @@ public class LangKeyType {
 		}
 	}
 	
-	public static class SQL {
+	public static class Game {
 		
-		protected static String		    type		        = "sql";
-		public static final LangKeyType	QUERY_FAILED		= new LangKeyType("query_failed", type, "ResultSet returned by query can not be null!");
-		public static final LangKeyType	CREATE_TABLE		= new LangKeyType("create_table", type, "Creating table for `%0%`");
-		public static final LangKeyType	CREATE_TABLE_FAILED	= new LangKeyType("create_table_failed", type, "Didn't created table!");
-		
-		public static List<LangKeyType> values() {
-			final List<LangKeyType> values = new ArrayList<>();
-			values.add(QUERY_FAILED);
-			return values;
+		public static class BedWars {
+			
+			protected static String			type			   = "game.bedwars";
+			public static final LangKeyType	BED_DESTROYED		= new LangKeyType("bed_destroyed", type, "The bed of team `%0%` was destroyed");
+			public static final LangKeyType	BED_TEAM_DESTROYED	= new LangKeyType("bed_team_destroyed", type, "The bed was destroyed");
+			public static final LangKeyType	OUT			       = new LangKeyType("out", type, "You are out, because your bed was destroyed!");
+			public static final LangKeyType	SB_OUT			   = new LangKeyType("sb_out", type, "`%0%` is out!");
+			public static final LangKeyType	START1			   = new LangKeyType("start1", type, "The game has started!");
+			public static final LangKeyType	START2			   = new LangKeyType("start2", type, "Collect ressources, trade and destroy the enemies team's bed!");
+			
+			public static List<LangKeyType> values() {
+				final List<LangKeyType> values = new ArrayList<>();
+				values.add(START1);
+				values.add(START2);
+				values.add(OUT);
+				values.add(SB_OUT);
+				values.add(BED_DESTROYED);
+				values.add(BED_TEAM_DESTROYED);
+				return values;
+			}
 		}
-	}
-	
-	public static class World {
+		public static class Crank {
+			
+			protected static String			type	= "game.crank";
+			public static final LangKeyType	START1	= new LangKeyType("star1", type, "You are posioned!");
+			public static final LangKeyType	START2	= new LangKeyType("start2", type, "Kill enemies, to keep your adraneline high!");
+			public static final LangKeyType	START3	= new LangKeyType("start3", type, "If it falls to low, you will die!");
+			
+			public static List<LangKeyType> values() {
+				final List<LangKeyType> values = new ArrayList<>();
+				values.add(START1);
+				values.add(START2);
+				values.add(START3);
+				return values;
+			}
+		}
+		public static class GetTheDrop {
+			
+			protected static String			type		= "game.getthedrop";
+			public static final LangKeyType	START1P1	= new LangKeyType("start1P1", type, "The first phase has started!");
+			public static final LangKeyType	START1P2	= new LangKeyType("start1P2", type, "The second phase has started!");
+			public static final LangKeyType	START2P1	= new LangKeyType("start2P1", type, "Collect drops and craft weapons and armor!");
+			public static final LangKeyType	START2P2	= new LangKeyType("start2P2", type, "Kill your enemies!");
+			public static final LangKeyType	START3P2	= new LangKeyType("start3P2", type, "Keep an eye out for drops on the altars!");
+			public static final LangKeyType	STOP1		= new LangKeyType("stop1", type, "The phase has ended!");
+			public static final LangKeyType	STOP2		= new LangKeyType("stop2", type, "The gods have dropped you some stuff!");
+			
+			public static List<LangKeyType> values() {
+				final List<LangKeyType> values = new ArrayList<>();
+				values.add(START1P1);
+				values.add(START2P1);
+				values.add(STOP1);
+				values.add(START1P2);
+				values.add(START2P2);
+				values.add(START3P2);
+				values.add(STOP2);
+				return values;
+			}
+		}
+		public static class OneInTheChamber {
+			
+			protected static String			type	= "game.oneinthechamber";
+			public static final LangKeyType	START1	= new LangKeyType("start1", type, "You have only one arrow left!");
+			public static final LangKeyType	START2	= new LangKeyType("start2", type, "Kill your enemies to earn new ones!");
+			public static final LangKeyType	START3	= new LangKeyType("start1", type, "You only have five lives!");
+			
+			public static List<LangKeyType> values() {
+				final List<LangKeyType> values = new ArrayList<>();
+				values.add(START1);
+				values.add(START2);
+				values.add(START3);
+				return values;
+			}
+		}
+		public static class UltraSpleef {
+			
+			protected static String			type	= "game.ultraspleef";
+			public static final LangKeyType	START	= new LangKeyType("start", type, "Use your GravityGun to move blocks and throw your enemies!");
+			
+			public static List<LangKeyType> values() {
+				final List<LangKeyType> values = new ArrayList<>();
+				values.add(START);
+				return values;
+			}
+		}
+		protected static String		    type		= "game";
 		
-		protected static String		    type		     = "world";
-		public static final LangKeyType	LOADING_CHUNKS		= new LangKeyType("loading_chunks", type, "Loading Chunks...");
-		public static final LangKeyType	CHUNKS_LOADED		= new LangKeyType("chunks_loaded", type, "`%0%` chunks loaded!");
-		public static final LangKeyType	CHUNK_LOAD_ERROR	= new LangKeyType("chunk_load_error", type, "Error while loading chunks: `%0%`!");
-		public static final LangKeyType	DELETE_OLD		 = new LangKeyType("delete_old", type, "Old map found, deleting...");
-		public static final LangKeyType	UNZIP		     = new LangKeyType("unzip", type, "Unzipping `%0%` to `%1%`");
-		public static final LangKeyType	LOADING_WORLD		= new LangKeyType("loading_world", type, "Loading world `%0%`...");
-		public static final LangKeyType	FILE_ERROR		 = new LangKeyType("file_error", type, "File exists with the name `%0%` and isn't a folder");
-		public static final LangKeyType	CONVERTING		 = new LangKeyType("converting", type, "Converting world `%0%`...");
-		public static final LangKeyType	CREATED		     = new LangKeyType("created", type, "Created world with dimension `%0%`");
+		public static final LangKeyType	END		 = new LangKeyType("end", type, "The game has ended!");
+		
+		public static final LangKeyType	TEAM_WIN	= new LangKeyType("team_win", type, "Your team has won!");
+		
+		public static final LangKeyType	TEAM_WON	= new LangKeyType("team_won", type, "The Team `%0%` has win!");
+		
+		public static final LangKeyType	WIN		 = new LangKeyType("win", type, "You have won!");
+		
+		public static final LangKeyType	WON		 = new LangKeyType("won", type, "The player `%0%` has won!");
 		
 		public static List<LangKeyType> values() {
 			final List<LangKeyType> values = new ArrayList<>();
-			values.add(LOADING_CHUNKS);
-			values.add(CHUNKS_LOADED);
-			values.add(CHUNK_LOAD_ERROR);
-			values.add(DELETE_OLD);
-			values.add(UNZIP);
-			values.add(LOADING_WORLD);
-			values.add(FILE_ERROR);
-			values.add(CONVERTING);
-			values.add(CREATED);
+			values.add(WIN);
+			values.add(WON);
+			values.add(END);
+			values.add(TEAM_WIN);
+			values.add(TEAM_WON);
+			values.addAll(UltraSpleef.values());
+			values.addAll(OneInTheChamber.values());
+			values.addAll(GetTheDrop.values());
+			values.addAll(Crank.values());
+			values.addAll(BedWars.values());
 			return values;
 		}
 	}
@@ -200,22 +273,22 @@ public class LangKeyType {
 		public static final LangKeyType	CHECK_LICENCE_FAILED	= new LangKeyType("check_licence_failed", type, "Licence check FAILED! (`%0%ms`)");
 		public static final LangKeyType	CHECK_LICENCE_PASSED	= new LangKeyType("check_licence_passed", type, "Licence check passed! (`%0%ms`)");
 		public static final LangKeyType	CHECK_UPDATES		 = new LangKeyType("check_licence", type, "Checking updater...");
+		public static final LangKeyType	DEACTIVATE		     = new LangKeyType("deactivate", type, "Deactivating");
+		public static final LangKeyType	DEACTIVATE_HANDLER		= new LangKeyType("deactivate_handler", type, "Deaktivating handlers...");
+		public static final LangKeyType	DEACTIVATED		     = new LangKeyType("deactivated", type, "Deactivated!");
 		public static final LangKeyType	ENABLE_LIBS		     = new LangKeyType("enable_libs", type, "Enabling external dependencies...");
 		public static final LangKeyType	ENABLE_LOGGING		 = new LangKeyType("enable_logging", type, "Enabling logging...");
 		public static final LangKeyType	ERROR		         = new LangKeyType("error", type, "Error: `%0%`");
+		public static final LangKeyType	LISTENER_NULL		 = new LangKeyType("listener_null", type, "A listener was null! Skipping...");
+		public static final LangKeyType	LOAD		         = new LangKeyType("load", type, "Loading `%0%`");
 		public static final LangKeyType	LOAD_DATA		     = new LangKeyType("load_data", type, "Load data...");
+		public static final LangKeyType	NOT_LOADED		     = new LangKeyType("not_loaded", type, "`%0%` not loaded!");
 		public static final LangKeyType	REGISTER_COMMANDS		= new LangKeyType("register_commands", type, "Registering commands...");
 		public static final LangKeyType	REGISTER_LISTENER		= new LangKeyType("register_listener", type, "Registering listener...");
 		public static final LangKeyType	SAVE_CONFIG		     = new LangKeyType("save_config", type, "Saving config...");
 		public static final LangKeyType	SEARCH_GAME		     = new LangKeyType("search_game", type, "Searching main game...");
-		public static final LangKeyType	DEACTIVATE		     = new LangKeyType("deactivate", type, "Deactivating");
-		public static final LangKeyType	UNREGISTER_LISTENER		= new LangKeyType("unregister_listener", type, "Unregister listener...");
 		public static final LangKeyType	STOP_TASKS		     = new LangKeyType("stop_tasks", type, "Stopping tasks...");
-		public static final LangKeyType	DEACTIVATE_HANDLER		= new LangKeyType("deactivate_handler", type, "Deaktivating handlers...");
-		public static final LangKeyType	DEACTIVATED		     = new LangKeyType("deactivated", type, "Deactivated!");
-		public static final LangKeyType	NOT_LOADED		     = new LangKeyType("not_loaded", type, "`%0%` not loaded!");
-		public static final LangKeyType	LOAD		         = new LangKeyType("load", type, "Loading `%0%`");
-		public static final LangKeyType	LISTENER_NULL		 = new LangKeyType("listener_null", type, "A listener was null! Skipping...");
+		public static final LangKeyType	UNREGISTER_LISTENER		= new LangKeyType("unregister_listener", type, "Unregister listener...");
 		
 		public static List<LangKeyType> values() {
 			final List<LangKeyType> values = new ArrayList<>();
@@ -266,117 +339,44 @@ public class LangKeyType {
 		}
 	}
 	
-	public static class Game {
+	public static class SQL {
 		
-		protected static String		    type		= "game";
-		public static final LangKeyType	WIN		 = new LangKeyType("win", type, "YOu have won!");
-		public static final LangKeyType	WON		 = new LangKeyType("won", type, "The player `%0%` has won!");
-		public static final LangKeyType	END		 = new LangKeyType("end", type, "The game has ended!");
-		public static final LangKeyType	TEAM_WIN	= new LangKeyType("team_win", type, "Your team has won!");
-		public static final LangKeyType	TEAM_WON	= new LangKeyType("team_won", type, "The Team `%0%` has win!");
-		
-		public static class UltraSpleef {
-			
-			protected static String			type	= "game.ultraspleef";
-			public static final LangKeyType	START	= new LangKeyType("start", type, "Use your GravityGun to move blocks and throw your enemies!");
-			
-			public static List<LangKeyType> values() {
-				final List<LangKeyType> values = new ArrayList<>();
-				values.add(START);
-				return values;
-			}
-		}
-		
-		public static class BedWars {
-			
-			protected static String			type			   = "game.bedwars";
-			public static final LangKeyType	START1			   = new LangKeyType("start1", type, "The game has started!");
-			public static final LangKeyType	START2			   = new LangKeyType("start2", type, "Collect ressources, trade and destroy the enemies team's bed!");
-			public static final LangKeyType	OUT			       = new LangKeyType("out", type, "You are out, because your bed was destroyed!");
-			public static final LangKeyType	SB_OUT			   = new LangKeyType("sb_out", type, "`%0%` is out!");
-			public static final LangKeyType	BED_DESTROYED		= new LangKeyType("bed_destroyed", type, "The bed of team `%0%` was destroyed");
-			public static final LangKeyType	BED_TEAM_DESTROYED	= new LangKeyType("bed_team_destroyed", type, "The bed was destroyed");
-			
-			public static List<LangKeyType> values() {
-				final List<LangKeyType> values = new ArrayList<>();
-				values.add(START1);
-				values.add(START2);
-				values.add(OUT);
-				values.add(SB_OUT);
-				values.add(BED_DESTROYED);
-				values.add(BED_TEAM_DESTROYED);
-				return values;
-			}
-		}
-		
-		public static class Crank {
-			
-			protected static String			type	= "game.crank";
-			public static final LangKeyType	START1	= new LangKeyType("star1", type, "You are posioned!");
-			public static final LangKeyType	START2	= new LangKeyType("start2", type, "Kill enemies, to keep your adraneline high!");
-			public static final LangKeyType	START3	= new LangKeyType("start3", type, "If it falls to low, you will die!");
-			
-			public static List<LangKeyType> values() {
-				final List<LangKeyType> values = new ArrayList<>();
-				values.add(START1);
-				values.add(START2);
-				values.add(START3);
-				return values;
-			}
-		}
-		
-		public static class OneInTheChamber {
-			
-			protected static String			type	= "game.oneinthechamber";
-			public static final LangKeyType	START1	= new LangKeyType("start1", type, "You have only one arrow left!");
-			public static final LangKeyType	START2	= new LangKeyType("start2", type, "Kill your enemies to earn new ones!");
-			public static final LangKeyType	START3	= new LangKeyType("start1", type, "You only have five lives!");
-			
-			public static List<LangKeyType> values() {
-				final List<LangKeyType> values = new ArrayList<>();
-				values.add(START1);
-				values.add(START2);
-				values.add(START3);
-				return values;
-			}
-		}
-		
-		public static class GetTheDrop {
-			
-			protected static String			type		= "game.getthedrop";
-			public static final LangKeyType	START1P1	= new LangKeyType("start1P1", type, "The first phase has started!");
-			public static final LangKeyType	START2P1	= new LangKeyType("start2P1", type, "Collect drops and craft weapons and armor!");
-			public static final LangKeyType	STOP1		= new LangKeyType("stop1", type, "The phase has ended!");
-			public static final LangKeyType	START1P2	= new LangKeyType("start1P2", type, "The second phase has started!");
-			public static final LangKeyType	START2P2	= new LangKeyType("start2P2", type, "Kill your enemies!");
-			public static final LangKeyType	START3P2	= new LangKeyType("start3P2", type, "Keep an eye out for drops on the altars!");
-			public static final LangKeyType	STOP2		= new LangKeyType("stop2", type, "The gods have dropped you some stuff!");
-			
-			public static List<LangKeyType> values() {
-				final List<LangKeyType> values = new ArrayList<>();
-				values.add(START1P1);
-				values.add(START2P1);
-				values.add(STOP1);
-				values.add(START1P2);
-				values.add(START2P2);
-				values.add(START3P2);
-				values.add(STOP2);
-				return values;
-			}
-		}
+		protected static String		    type		        = "sql";
+		public static final LangKeyType	CREATE_TABLE		= new LangKeyType("create_table", type, "Creating table for `%0%`");
+		public static final LangKeyType	CREATE_TABLE_FAILED	= new LangKeyType("create_table_failed", type, "Didn't created table!");
+		public static final LangKeyType	QUERY_FAILED		= new LangKeyType("query_failed", type, "ResultSet returned by query can not be null!");
 		
 		public static List<LangKeyType> values() {
 			final List<LangKeyType> values = new ArrayList<>();
-			values.add(WIN);
-			values.add(WON);
-			values.add(END);
-			values.add(TEAM_WIN);
-			values.add(TEAM_WON);
-			values.addAll(UltraSpleef.values());
-			values.addAll(OneInTheChamber.values());
-			values.addAll(GetTheDrop.values());
-			values.addAll(Crank.values());
-			values.addAll(BedWars.values());
+			values.add(QUERY_FAILED);
+			return values;
+		}
+	}
+	
+	public static class World {
+		
+		protected static String		    type		     = "world";
+		public static final LangKeyType	CHUNK_LOAD_ERROR	= new LangKeyType("chunk_load_error", type, "Error while loading chunks: `%0%`!");
+		public static final LangKeyType	CHUNKS_LOADED		= new LangKeyType("chunks_loaded", type, "`%0%` chunks loaded!");
+		public static final LangKeyType	CONVERTING		 = new LangKeyType("converting", type, "Converting world `%0%`...");
+		public static final LangKeyType	CREATED		     = new LangKeyType("created", type, "Created world with dimension `%0%`");
+		public static final LangKeyType	DELETE_OLD		 = new LangKeyType("delete_old", type, "Old map found, deleting...");
+		public static final LangKeyType	FILE_ERROR		 = new LangKeyType("file_error", type, "File exists with the name `%0%` and isn't a folder");
+		public static final LangKeyType	LOADING_CHUNKS		= new LangKeyType("loading_chunks", type, "Loading Chunks...");
+		public static final LangKeyType	LOADING_WORLD		= new LangKeyType("loading_world", type, "Loading world `%0%`...");
+		public static final LangKeyType	UNZIP		     = new LangKeyType("unzip", type, "Unzipping `%0%` to `%1%`");
+		
+		public static List<LangKeyType> values() {
+			final List<LangKeyType> values = new ArrayList<>();
+			values.add(LOADING_CHUNKS);
+			values.add(CHUNKS_LOADED);
+			values.add(CHUNK_LOAD_ERROR);
+			values.add(DELETE_OLD);
+			values.add(UNZIP);
+			values.add(LOADING_WORLD);
+			values.add(FILE_ERROR);
+			values.add(CONVERTING);
+			values.add(CREATED);
 			return values;
 		}
 	}
