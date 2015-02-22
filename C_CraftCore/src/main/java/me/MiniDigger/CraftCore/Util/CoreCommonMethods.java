9699b4ee3f028dd5b67c0387ff6ffaf418a06330
MiniDigger <admin@minidigger.me>
2015-02-22 11:46:42 +0100

    Reactivate licence killing, only one licen per server (far more easy to do :D)

--- C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Util/CoreCommonMethods.java
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Util/CoreCommonMethods.java
@@ -108,10 +108,10 @@ public class CoreCommonMethods implements CommonMethods {
 	
 	@Override
 	public void killPlugin() {
-		System.out.println("Licene check failed but I don't care as long as you registered it at startup");
-		// System.out.println("The Plugin decided to kill itselve");
-		// System.out.println("This mostly happens, if your licence is wrong, or a other server uses the same licence");
-		// Bukkit.getPluginManager().disablePlugin(Core.getCore().getInstance());
+		// System.out.println("Licene check failed but I don't care as long as you registered it at startup");
+		System.out.println("The Plugin decided to kill itselve");
+		System.out.println("This mostly happens, if your licence is wrong, or a other server uses the same licence");
+		Bukkit.getPluginManager().disablePlugin(Core.getCore().getInstance());
 	}
 	
 	@Override
