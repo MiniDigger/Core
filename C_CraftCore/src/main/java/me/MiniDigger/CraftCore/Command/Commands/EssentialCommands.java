2cefa998730a8a9d29897eedc8707790d237c3a0
MiniDigger <admin@minidigger.me>
2015-05-12 21:06:54 +0200

    Add killall cmd

--- C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Command/Commands/EssentialCommands.java
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Command/Commands/EssentialCommands.java
@@ -22,12 +22,15 @@ package me.MiniDigger.CraftCore.Command.Commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 
 import me.MiniDigger.Core.Core;
 import me.MiniDigger.Core.Command.Command;
 import me.MiniDigger.Core.Command.CommandArgs;
 import me.MiniDigger.Core.Prefix.Prefix;
+import me.MiniDigger.Core.Util.EntityUtil.Type;
 
 /**
  * @author Martin
@@ -35,7 +38,7 @@ import me.MiniDigger.Core.Prefix.Prefix;
  */
 public class EssentialCommands {
 	
-	@Command(name = "spawnmob", usage = "spawnmob <type> [count]", min = 1, max = 2, consol = false, permission = "spawnmob")
+	@Command(name = "spawnmob", usage = "spawnmob <type> [count]", min = 1, max = 2, consol = false, permission = "spawnmob", description = "Spawn Mobs")
 	public void spawnmob(final CommandArgs args) {
 		int c = 1;
 		final EntityType type;
@@ -60,9 +63,30 @@ public class EssentialCommands {
 				for (int i = 0; i < fC; i++) {
 					args.getPlayer().getWorld().spawn(args.getPlayer().getLocation(), type.getEntityClass());
 				}
-				Prefix.API.getPrefix().then("Es wurde" + (fC > 1 ? "n " : " ") + fC + " " + type.name() + " gespawnt!").color(ChatColor.GREEN).send(args.getPlayer());;
+				Prefix.API.getPrefix().then("Es wurde" + (fC > 1 ? "n " : " ") + fC + " " + type.name() + " gespawnt!").color(ChatColor.GREEN).send(args.getPlayer());
 			}
 			
 		});
 	}
+	
+	@Command(name = "killall", usage = "", min = 0, max = 0, consol = true, permission = "killall", description = "Tötet Mobs")
+	public void killall(final CommandArgs args) {
+		int i = 0;
+		for (World world : Bukkit.getWorlds()) {
+			for (Entity e : world.getEntities()) {
+				boolean b = false;
+				for (EntityType t : Core.getCore().getEntityUtil().getAll(Type.ANGRY, Type.FRIENDLY, Type.PASSIV, Type.PROJECTILE)) {
+					if (t == e.getType()) {
+						b = true;
+					}
+				}
+				if (b) {
+					i++;
+					e.remove();
+				}
+			}
+		}
+		Prefix.API.getPrefix().then("Es wurde" + (i > 1 ? "n " : " ") + i + " Entities gelöscht!").color(ChatColor.GREEN).send(args.getSender());
+	}
+	
 }
