d4bea37c253d7f6210da0fd7767a12d90a6decc0
MiniDigger <admin@minidigger.me>
2015-06-08 17:50:03 +0200

    A bit SG stuff

--- C_SurvivalGames/src/main/java/me/MiniDigger/Core/AddOn/SurvivalGames/SGFeature.java
+++ C_SurvivalGames/src/main/java/me/MiniDigger/Core/AddOn/SurvivalGames/SGFeature.java
@@ -3,10 +3,16 @@ package me.MiniDigger.Core.AddOn.SurvivalGames;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.DyeColor;
+import org.bukkit.WorldBorder;
+
 import me.MiniDigger.Core.Feature.FeatureType;
+import me.MiniDigger.Core.Map.MapData;
 import me.MiniDigger.Core.Phase.Phase;
 
 import me.MiniDigger.CraftCore.Feature.CoreFeature;
+import me.MiniDigger.CraftCore.Feature.Features.MapFeature;
 
 public class SGFeature extends CoreFeature {
 	
@@ -36,6 +42,12 @@ public class SGFeature extends CoreFeature {
 	
 	@Override
 	public void start() {
+		MapData m = ((MapFeature) getPhase().getFeature(FeatureType.MAP)).getMap();
+		WorldBorder b = Bukkit.getWorld(m.getName()).getWorldBorder();
+		b.setCenter(m.getLocs(DyeColor.BLACK).values().iterator().next());
+		b.setSize(b.getCenter().distance(m.getLocs(DyeColor.BLUE).values().iterator().next()),0);
+		b.setDamageBuffer(5.0);
+		b.setWarningDistance(5);
 		
 	}
 	
--- C_SurvivalGames/src/main/java/me/MiniDigger/Core/AddOn/SurvivalGames/SGPhase.java
+++ C_SurvivalGames/src/main/java/me/MiniDigger/Core/AddOn/SurvivalGames/SGPhase.java
@@ -1,8 +1,29 @@
 package me.MiniDigger.Core.AddOn.SurvivalGames;
 
-import me.MiniDigger.Core.Game.Game;
+import org.bukkit.WeatherType;
 
+import me.MiniDigger.Core.Core;
+import me.MiniDigger.Core.Feature.FeatureType;
+import me.MiniDigger.Core.Game.Game;
+import me.MiniDigger.Core.Lang.LangKeyType;
+import me.MiniDigger.Core.Lang.MsgType;
+import me.MiniDigger.Core.Util.EntityUtil.Type;
+
+import me.MiniDigger.CraftCore.Feature.Features.AutoRespawnFeature;
+import me.MiniDigger.CraftCore.Feature.Features.BleedFeature;
+import me.MiniDigger.CraftCore.Feature.Features.DropFeature;
+import me.MiniDigger.CraftCore.Feature.Features.FixedTimeFeature;
+import me.MiniDigger.CraftCore.Feature.Features.FixedWeatherFeature;
+import me.MiniDigger.CraftCore.Feature.Features.LastManStandingFeature;
+import me.MiniDigger.CraftCore.Feature.Features.MapFeature;
+import me.MiniDigger.CraftCore.Feature.Features.MobFeature;
+import me.MiniDigger.CraftCore.Feature.Features.NoDropFeature;
+import me.MiniDigger.CraftCore.Feature.Features.PvPFeature;
+import me.MiniDigger.CraftCore.Feature.Features.SpawnFeature;
+import me.MiniDigger.CraftCore.Feature.Features.SpecateFeature;
 import me.MiniDigger.CraftCore.Phase.CorePhase;
+
+import mkremins.fanciful.FancyMessage;
 
 public class SGPhase extends CorePhase {
 	
@@ -27,7 +48,25 @@ public class SGPhase extends CorePhase {
 	
 	@Override
 	public void init() {
-		
+		addFeature(new BleedFeature(this));
+		addFeature(new AutoRespawnFeature(this));
+		addFeature(new LastManStandingFeature(this));
+		addFeature(new MobFeature(this, Core.getCore().getEntityUtil().getAll(Type.OTHER, Type.CART, Type.PROJECTILE, Type.UTILITY)));
+		addFeature(new FixedTimeFeature(this, 12000));
+		addFeature(new FixedWeatherFeature(this, WeatherType.CLEAR));
+		addFeature(new MapFeature(this, null, false));
+		addFeature(new PvPFeature(this, true));
+		addFeature(new SpawnFeature(this, false));
+		addFeature(new SpecateFeature(this));
+		addFeature(new NoDropFeature(this));
 	}
 	
+	@Override
+	public void startPhase() {
+		getGame().broadCastMessage(new FancyMessage("Lasset die Spiele beginnen!"));
+		final String winner = getGame().getGameData("VoteWinner");
+		
+		((MapFeature) getFeature(FeatureType.MAP)).setMap(winner);
+		super.startPhase();
+	}
 }
