167e7074ecd70e925fb059d18819649854fb5a72
MiniDigger <admin@minidigger.me>
2015-08-02 21:01:56 +0200

    Swap args in kit give completer

--- C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Command/Completer/KitCompleter.java
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Command/Completer/KitCompleter.java
@@ -30,80 +30,80 @@ import me.MiniDigger.Core.Kit.Kit;
 import me.MiniDigger.Core.User.User;
 
 public class KitCompleter {
-	
+
 	@Completer(name = "kit")
 	public List<String> kit(final CommandArgs args) {
 		final List<String> result = new ArrayList<>();
-		
+
 		if (args.getArgs().length == 1) {
 			result.add("create");
 			result.add("give");
 			result.add("modify");
 			result.add("delete");
-			
+
 			return Core.getCore().getCommonMethods().completer(result, args.getArgs()[0]);
 		} else {
 			return new ArrayList<String>();
 		}
 	}
-	
+
 	@Completer(name = "kit.create")
 	public List<String> create(final CommandArgs args) {
 		final List<String> result = new ArrayList<>();
-		
+
 		result.add("");
-		
+
 		return result;
 	}
-	
+
 	@Completer(name = "kit.give")
 	public List<String> give(final CommandArgs args) {
 		final List<String> result = new ArrayList<>();
-		
-		if (args.getArgs().length == 1) {
+
+		if (args.getArgs().length == 2) {
 			for (final User user : Core.getCore().getUserHandler().getOnlineUsers()) {
 				result.add(user.getDisplayName());
 			}
-			
+
 			return Core.getCore().getCommonMethods().completer(result, args.getArgs()[0]);
-		} else if (args.getArgs().length == 2) {
+		} else if (args.getArgs().length == 1) {
 			for (final Kit k : Core.getCore().getKitHandler().getKits()) {
 				result.add(k.getName());
 			}
-			
+
 			return Core.getCore().getCommonMethods().completer(result, args.getArgs()[1]);
 		} else {
 			return new ArrayList<String>();
 		}
 	}
-	
+
 	@Completer(name = "kit.modify")
 	public List<String> modify(final CommandArgs args) {
 		final List<String> result = new ArrayList<>();
-		
+
 		if (args.getArgs().length == 1) {
 			for (final Kit k : Core.getCore().getKitHandler().getKits()) {
 				result.add(k.getName());
 			}
-			
+
 			return Core.getCore().getCommonMethods().completer(result, args.getArgs()[0]);
 		}
-		
+
 		return result;
 	}
-	
+
 	@Completer(name = "kit.delete")
 	public List<String> delete(final CommandArgs args) {
 		final List<String> result = new ArrayList<>();
-		
+
 		if (args.getArgs().length == 1) {
 			for (final Kit k : Core.getCore().getKitHandler().getKits()) {
 				result.add(k.getName());
 			}
-			
+
 			return Core.getCore().getCommonMethods().completer(result, args.getArgs()[0]);
 		}
-		
+
 		return result;
 	}
 }
