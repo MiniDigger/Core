e197ef9dfbb9c1b7c4dc5ac2ba22497e98cf5663
MiniDigger <admin@minidigger.me>
2015-08-02 14:17:20 +0200

    Fix core/corebugs#129 (finaly) (better not remove player entities...)

--- C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Feature/Features/MapResetFeature.java
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Feature/Features/MapResetFeature.java
@@ -26,6 +26,7 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockFormEvent;
@@ -40,94 +41,96 @@ import me.MiniDigger.Core.Phase.Phase;
 import me.MiniDigger.CraftCore.Feature.CoreFeature;
 
 public class MapResetFeature extends CoreFeature {
-	
-	private final List<BlockState>	changes	= new ArrayList<BlockState>();
-	private String					map		= "";
-	
+
+	private final List<BlockState> changes = new ArrayList<BlockState>();
+	private String map = "";
+
 	public MapResetFeature(final Phase phase) {
 		super(phase);
 	}
-	
+
 	@Override
 	public FeatureType getType() {
 		return FeatureType.MAPRESET;
 	}
-	
+
 	@Override
 	public List<FeatureType> getDependencies() {
 		return new ArrayList<FeatureType>();
 	}
-	
+
 	@Override
 	public List<FeatureType> getSoftDependencies() {
 		return new ArrayList<FeatureType>();
 	}
-	
+
 	@Override
 	public List<FeatureType> getIncompabilities() {
 		return new ArrayList<FeatureType>();
 	}
-	
+
 	@Override
 	public void start() {
 		map = ((MapFeature) getPhase().getFeature(FeatureType.MAP)).getMap().getName();
 	}
-	
+
 	@Override
 	public void end() {
 		for (int i = changes.size() - 1; i >= 0; i--) {
 			changes.get(i).update(true, false);
 		}
-		
+
 		for (Entity e : Bukkit.getWorld(map).getEntities()) {
-			e.remove();
+			if (e.getType() != EntityType.PLAYER) {
+				e.remove();
+			}
 		}
 	}
-	
+
 	public void add(final BlockState s) {
 		changes.add(s);
 	}
-	
+
 	@EventHandler
 	public void onPlace(final BlockPlaceEvent e) {
 		if (getPhase().getGame().getPlayers().contains(e.getPlayer().getUniqueId())) {
 			add(e.getBlockReplacedState());
 		}
 	}
-	
+
 	@EventHandler
 	public void onDestroy(final BlockBreakEvent e) {
 		if (getPhase().getGame().getPlayers().contains(e.getPlayer().getUniqueId())) {
 			add(e.getBlock().getState());
 		}
 	}
-	
+
 	@EventHandler
 	public void onForm(final BlockFormEvent e) {
 		if (e.getBlock().getWorld().getName().equalsIgnoreCase(map)) {
 			add(e.getBlock().getState());
 		}
 	}
-	
+
 	@EventHandler
 	public void onExtend(final BlockPistonExtendEvent e) {
 		if (e.getBlock().getWorld().getName().equalsIgnoreCase(map)) {
 			e.setCancelled(true);
 		}
 	}
-	
+
 	@EventHandler
 	public void onRetrac(final BlockPistonRetractEvent e) {
 		if (e.getBlock().getWorld().getName().equalsIgnoreCase(map)) {
 			e.setCancelled(true);
 		}
 	}
-	
+
 	@EventHandler
 	public void onForm(final BlockPhysicsEvent e) {
 		if (e.getBlock().getWorld().getName().equalsIgnoreCase(map)) {
 			add(e.getBlock().getState());
 		}
 	}
-	
+
 }
