deb73887336b48dc44fd1c1929d26440e87df4f5
MiniDigger <admin@minidigger.me>
2015-08-03 16:26:56 +0200

    Some kitpvp stuff

--- C_KitPvP/src/main/java/me/MiniDigger/Core/AddOn/KitPvP/KPPhase.java
+++ C_KitPvP/src/main/java/me/MiniDigger/Core/AddOn/KitPvP/KPPhase.java
@@ -20,34 +20,72 @@
  */
 package me.MiniDigger.Core.AddOn.KitPvP;
 
-import me.MiniDigger.Core.Game.Game;
+import org.bukkit.WeatherType;
 
+import me.MiniDigger.Core.Core;
+import me.MiniDigger.Core.Game.Game;
+import me.MiniDigger.Core.Util.EntityUtil.Type;
+import me.MiniDigger.CraftCore.Feature.Features.AutoRespawnFeature;
+import me.MiniDigger.CraftCore.Feature.Features.BleedFeature;
+import me.MiniDigger.CraftCore.Feature.Features.FixedFoodFeature;
+import me.MiniDigger.CraftCore.Feature.Features.FixedTimeFeature;
+import me.MiniDigger.CraftCore.Feature.Features.FixedWeatherFeature;
+import me.MiniDigger.CraftCore.Feature.Features.KillsFeature;
+import me.MiniDigger.CraftCore.Feature.Features.KitFeature;
+import me.MiniDigger.CraftCore.Feature.Features.MapFeature;
+import me.MiniDigger.CraftCore.Feature.Features.MobFeature;
+import me.MiniDigger.CraftCore.Feature.Features.NoDropFeature;
+import me.MiniDigger.CraftCore.Feature.Features.NoPickupFeature;
+import me.MiniDigger.CraftCore.Feature.Features.PvPFeature;
+import me.MiniDigger.CraftCore.Feature.Features.SpawnFeature;
+import me.MiniDigger.CraftCore.Feature.Features.SpecateFeature;
 import me.MiniDigger.CraftCore.Phase.CorePhase;
 
 public class KPPhase extends CorePhase {
-	
+
 	public KPPhase(final Game game) {
 		super(game, null);
 	}
-	
+
 	@Override
 	public String getName() {
 		return "KitPvP";
 	}
-	
+
 	@Override
 	public boolean displayBar() {
 		return true;
 	}
-	
+
 	@Override
 	public boolean displayLevel() {
 		return false;
 	}
-	
+
 	@Override
 	public void init() {
-
+		addFeature(new BleedFeature(this));
+		addFeature(new AutoRespawnFeature(this));
+		addFeature(new FixedFoodFeature(this));
+		addFeature(new MobFeature(this,
+				Core.getCore().getEntityUtil().getAll(Type.OTHER, Type.CART, Type.PROJECTILE, Type.UTILITY)));
+		addFeature(new FixedTimeFeature(this, 6000));
+		addFeature(new FixedWeatherFeature(this, WeatherType.CLEAR));
+		addFeature(new MapFeature(this, null, true));
+		addFeature(new PvPFeature(this, true));
+		addFeature(new SpawnFeature(this, false));
+		addFeature(new KPFeature(this));
+		addFeature(new SpecateFeature(this));
+		addFeature(new KitFeature(this));
+		addFeature(new KillsFeature(this, -1, true));
+		addFeature(new NoPickupFeature(this));
+		addFeature(new NoDropFeature(this));
 	}
-	
+
+	@Override
+	public void startPhase() {
+		// TODO Auto-generated method stub
+		getGame().setAllowJoin(true);
+		super.startPhase();
+	}
 }
