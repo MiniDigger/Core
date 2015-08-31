82531adf060bbd0772ee9fa4f6104b5a4a8890a1
MiniDigger <admin@minidigger.me>
2015-08-31 16:31:33 +0200

    moar implementations

--- C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Stats/CoreActionListener.java
+++ C_CraftCore/src/main/java/me/MiniDigger/CraftCore/Stats/CoreActionListener.java
@@ -1,8 +1,13 @@
 package me.MiniDigger.CraftCore.Stats;
 
+import org.bukkit.block.Chest;
+import org.bukkit.block.DoubleChest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
@@ -11,6 +16,8 @@ import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import me.MiniDigger.Core.Stats.ActionType;
+
+import me.MiniDigger.CraftCore.Event.Events.CoreUserChatEvent;
 
 public class CoreActionListener implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR)
@@ -47,5 +54,35 @@ public class CoreActionListener implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onLeave(PlayerQuitEvent e) {
 		new CoreAction(ActionType.LEAVE, "", e.getPlayer().getUniqueId()).submit();
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onBucketFill(PlayerBucketFillEvent e) {
+		if (!e.isCancelled()) {
+			new CoreAction(ActionType.BUCKET_FILLED, e.getBucket().name(), e.getPlayer().getUniqueId()).submit();
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onBucketFill(PlayerBucketEmptyEvent e) {
+		if (!e.isCancelled()) {
+			new CoreAction(ActionType.BUCKET_EMPTIED, e.getBucket().name(), e.getPlayer().getUniqueId()).submit();
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onChestOpen(InventoryOpenEvent e) {
+		if (e.getInventory().getHolder() instanceof Chest || e.getInventory().getHolder() instanceof DoubleChest) {
+			if (!e.isCancelled()) {
+				new CoreAction(ActionType.CHEST_OPENED, "", e.getPlayer().getUniqueId()).submit();
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onChat(CoreUserChatEvent e) {
+		if (!e.isCancelled()) {
+			new CoreAction(ActionType.LINE_CHATTED, e.getMsg(), e.getUser().getUUID()).submit();
+		}
 	}
 }
